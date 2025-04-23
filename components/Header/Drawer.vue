a69b0c0e379bf5fbce2e5590a1d0186c3c81e005
shayanbaba22 <shayan.rtl@gmail.com>
2025-04-23 12:32:54 +0330

    drawer z index bug

--- components/Header/Drawer.vue
+++ components/Header/Drawer.vue
@@ -3,15 +3,14 @@ import Logo from "@/public/logo.svg";
 import Close from "@/assets/icons/Close";
 import NavigationItems from "@/components/Header/NavigationItems.vue";
 
-const toggleMenu  = inject("toggleMenuProvider");
-
+const toggleMenu = inject("toggleMenuProvider");
 </script>
 
 <template>
   <div
-    class="flex flex-col-reverse rounded-e-3xl shadow-xl fixed z-50 top-0 right-0 bg-white w-60 h-full justify-end p-4 transition-all duration-500 border border-[#eee] lg:hidden"
+    class="flex flex-col-reverse rounded-e-3xl shadow-xl fixed z-[999px] top-0 right-0 bg-white w-60 h-full justify-end p-4 transition-all duration-500 border border-[#eee] lg:hidden"
   >
-    <NuxtImage
+    <NuxtImg
       :src="Logo"
       alt="مرکز جامع توانبخشی ایران"
       class="order-2 lg:order-1 mb-6"
